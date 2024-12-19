e6dcebcd9bfeba86d962a6aea831c18c86227246
developer-shahrukh <shahrukh4854@gmail.com>
2024-12-20 00:49:35 +0530

    Many components list created

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[{ hostname: "images.pexels.com"}]
+  },
   webpack: (config, { dev }) => {
     if (dev) {
       config.cache = false; // Disable caching in development
